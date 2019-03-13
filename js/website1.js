8c79a1d77228dbfbed99e8c824b00c0c51605f32
unknown <daishagolden430@gmail.com>
2019-03-13 11:20:53 -0500

    blue wireframe

---
+++ js/website1.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slide').slick({
+      infinite: true,
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 3, 
+      autoplay: true,
+      autoplaySpeed: 3000,
+      arrows: false,
+    });   
+  });
